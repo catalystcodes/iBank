3c382ad66c5ebb0939b64bb6d7bb1c367804ccab
catalystcodes <muh4mub@gmail.com>
2025-10-21 01:49:40 +0100

    feat: setUp

---
+++ app/(auth)/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const SignIn = () => {
+  return <View>{/* SignIn Screen */}</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignIn;
---
+++ app/(auth)/SignUp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const SignUp = () => {
+  return <View>{/* SignUp Screen */}</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignUp;
